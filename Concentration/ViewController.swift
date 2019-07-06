4d54516aaa369120877f22d0729aa4763c539e98
yixuanqi <yixuan99316@gmail.com>
2019-07-06 10:07:35 -0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Yi Xuan Qi on 2019-07-06.
+//  Copyright © 2019 Yi Xuan Qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
